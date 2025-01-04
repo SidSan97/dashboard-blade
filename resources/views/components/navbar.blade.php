8990ac84cbff3e976081a09ef822b5c73bf1c0a2
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-01-04 19:53:28 -0300

    Ajustes no navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,20 +6,30 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse flex-row-reverse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav me-auto sidebar-links d-lg-none">
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-person-fill"></i> Social Users</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-file-earmark-text"></i> Campaigns</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-person-circle"></i> Avatars</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-file-play"></i> Video Files</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-gear-fill"></i> Resources</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-film"></i> Status Video Generation</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-list-task"></i> Task Queue</a></li>
+            </ul>
+
+            <div class="navbar-nav ms-auto">
                 <a class="nav-link" aria-current="page" href="#">
                     WEDNESDAY, <strong>SEPTEMBER</strong>
                     12, 2024
                 </a>
-
                 <a class="nav-link notification" href="#">
                     <i class="bi bi-bell-fill notification-bell"></i>
                     <div class="notification-badge">+10</div>
                 </a>
-
-                <a class="nav-link" href="#">Will Lennon</a>
-                <img src="{{asset('img/man.jpg')}}" alt="Profile Picture" class="profile-picture">
+                <div class="d-flex align-items-center">
+                    <a class="nav-link" href="#">Will Lennon</a>
+                    <img src="{{asset('img/man.jpg')}}" alt="Profile Picture" class="profile-picture">
+                </div>
             </div>
         </div>
     </div>
