98c028d232f5baf56ea48282378b256ef5f3ad29
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-01-30 23:57:56 -0300

    Criação da página de criaçao de campaigns

---
+++ resources/views/components/social-media-plataform.blade.php
@@ -0,0 +1,12 @@
+<div class="card-dashboard mt-2 p-0 mb-4">
+    <div class="social-media-plataform">
+        <div class="container pt-3">
+            <div class="row">
+                <div class="col-12 d-flex mb-2">
+                    <div class="logo">#</div>
+                    <span class="card-title">SOCIAL MEDIA PLATAFORM</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,38 +10,52 @@
                 <div class="col-12">
                     @include('components/sidebar')
 
-                    <main class="main-content">
-                        <div class="container-fluid p-0">
-                            <div class="row mb-2">
-                                <div class="col-lg-9">
-                                    @include('components/overviews')
+                    @if (Route::is('home'))
+                        <main class="main-content">
+                            <div class="container-fluid p-0">
+                                <div class="row mb-2">
+                                    <div class="col-lg-9">
+                                        @include('components/overviews')
+                                    </div>
+
+                                    <div class="col-lg-3 d-flex flex-column justify-content-between">
+                                        @include('components/total-earned')
+
+                                        @include('components/total-invested')
+                                    </div>
                                 </div>
 
-                                <div class="col-lg-3 d-flex flex-column justify-content-between">
-                                    @include('components/total-earned')
+                                <div class="row mb-2">
+                                    <div class="col-lg-6 mb-2">
+                                        @include('components/campaigns')
 
-                                    @include('components/total-invested')
+                                        @include('components/published-videos')
+                                        <br><br>
+                                    </div>
+
+                                    <div class="col lg-6 mb-2">
+                                        @include('components/total-views')
+
+                                        @include('components/top-social-users')
+
+                                        @include('components/posts')
+                                    </div>
                                 </div>
                             </div>
+                        </main>
+                    @endif
 
-                            <div class="row mb-2">
-                                <div class="col-lg-6 mb-2">
-                                    @include('components/campaigns')
-
-                                    @include('components/published-videos')
-                                    <br><br>
-                                </div>
-
-                                <div class="col lg-6 mb-2">
-                                    @include('components/total-views')
-
-                                    @include('components/top-social-users')
-
-                                    @include('components/posts')
+                    @if (Route::is('campaigns'))
+                        <main class="main-create-campaigns-view">
+                            <div class="container-fluid p-0">
+                                <div class="row mb-2">
+                                    <div class="col-lg-6 mb-2">
+                                        @include('components/social-media-plataform')
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </main>
+                        </main>
+                    @endif
                 </div>
             </div>
         </div>
